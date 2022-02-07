7a586ab2e15cbe9f3b13e7101271e563b89cc230
RoUchiha <swaggsikh@gmail.com>
2022-02-07 00:27:37 -0600

    commit tree 1d7559bc270397cac310d8cdf2a9dae107805ba6

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :rides
+  resources :attractions
+  resources :users
+  get '/signin', to: 'session#new', as: 'signin'
+  post '/session', to: 'session#create', as: 'session'
+  delete '/session/', to: 'session#destroy', as: 'session_delete'
+  root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
